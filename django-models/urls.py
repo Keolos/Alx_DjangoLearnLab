e518efd6d59e7095b9bb765c02c4eee54d81806c
Keolos <abdullahmohammednaji@gmail.com>
2025-07-14 19:33:24 +0000

    a

--- django-models/urls.py
+++ django-models/urls.py
@@ -6,3 +6,18 @@ urlpatterns = [
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
     path('', include('relationship_app.urls')),
 ]
+
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
+
+urlpatterns = [
+    # Book & library views
+    path('books/', views.list_books, name='list_books'),
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+
+    # Auth views
+    path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('register/', views.register, name='register'),
+]
