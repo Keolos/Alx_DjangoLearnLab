9340116d9ec7682d3209c450891a3bf0907bfd24
Mohammed Naji Abdullah <abdullahmohammednaji@gmail.com>
2025-07-19 21:33:34 +0000

    done a lot

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Book, Library  # Import both models
 
-# Create your views here.
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
+def library_detail(request, library_id):
+    library = get_object_or_404(Library, id=library_id)
+    return render(request, 'relationship_app/library_detail.html', {'library': library})
