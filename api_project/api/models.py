82838fbe7cdc3b93bdf08f30fdf7edc956af660c
Mohammed Naji Abdullah <abdullahmohammednaji@gmail.com>
2025-08-03 13:19:37 +0000

    created api_project

---
+++ api_project/api/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+class book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    published_date = models.DateField()
+    isbn = models.CharField(max_length=13, unique=True)
+
+    def __str__(self):
+        return self.title
+    
+
+class author(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.name
+    class Meta:
+        permissions = [
+            ("can_add_author", "Can add author"),
+            ("can_change_author", "Can change author"),
+            ("can_delete_author", "Can delete author"),
+        ]
