9014c4af636ece338114012098772519e50d3ed1
Mohammed Naji Abdullah <abdullahmohammednaji@gmail.com>
2025-08-24 11:34:30 +0000

    a

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -36,4 +36,12 @@ urlpatterns = [
          CommentUpdateView.as_view(), name='comment-update'),
     path('comment/<int:pk>/delete/',
          CommentDeleteView.as_view(), name='comment-delete'),
+    # comment urls
+    path('post/<int:pk>/comments/new/',
+         CommentCreateView.as_view(), name='comment-create'),
+    path('comment/<int:pk>/update/',
+         CommentUpdateView.as_view(), name='comment-update'),
+    path('comment/<int:pk>/delete/',
+         CommentDeleteView.as_view(), name='comment-delete'),
+
 ]
