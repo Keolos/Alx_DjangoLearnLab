015090cda02b28e9364e32bddeeb98c5b665a9ad
Mohammed Naji Abdullah <abdullahmohammednaji@gmail.com>
2025-08-24 11:33:03 +0000

    a

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -3,6 +3,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Profile
+from .models import Comment
 
 
 class CustomUserCreationForm(UserCreationForm):
@@ -31,3 +32,9 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ["title", "content"]
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']  # include only fields you want users to fill
