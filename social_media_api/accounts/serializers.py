4a60e9e6ba6abc12b1c12f68a47b17e51e02c71e
Mohammed Naji Abdullah <abdullahmohammednaji@gmail.com>
2025-08-31 10:28:58 +0000

    Refactor serializers to use CustomUser model and enhance validation for email uniqueness

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,13 +1,13 @@
+from .apps import AccountsConfig
+from django.apps import AppConfig
 from rest_framework import serializers
-from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
-
-User = get_user_model()
+from .models import CustomUser, User
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
+        model = CustomUser
         fields = ['id', 'username', 'email',
                   'bio', 'profile_picture', 'followers']
 
@@ -16,11 +16,11 @@ class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     class Meta:
-        model = User
+        model = CustomUser
         fields = ['username', 'email', 'password', 'bio', 'profile_picture']
 
     def create(self, validated_data):
-        user = User.objects.create_user(
+        user = CustomUser.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
             password=validated_data['password'],
@@ -36,3 +36,71 @@ class RegisterSerializer(serializers.ModelSerializer):
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField(write_only=True)
+    token = serializers.CharField(read_only=True)
+    user_id = serializers.IntegerField(read_only=True)
+    username = serializers.CharField(read_only=True)
+    email = serializers.EmailField(read_only=True)
+    bio = serializers.CharField(read_only=True)
+    profile_picture = serializers.ImageField(read_only=True)
+    followers = serializers.PrimaryKeyRelatedField(
+        many=True, read_only=True)
+
+    def validate(self, data):
+        from django.contrib.auth import authenticate
+        user = authenticate(
+            username=data.get('username'), password=data.get('password'))
+        if user is None:
+            raise serializers.ValidationError("Invalid credentials")
+        token, created = Token.objects.get_or_create(user=user)
+        return {
+            'token': token.key,
+            'user_id': user.id,
+            'username': user.username,
+            'email': user.email,
+            'bio': user.bio,
+            'profile_picture': user.profile_picture,
+            'followers': user.followers.all(),
+        }
+        return data
+
+
+# accounts/apps.py
+
+
+class AccountsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'accounts'
+
+
+class AccountsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'accounts'
+
+    def ready(self):
+        from . import signals  # noqa
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'email', 'password', 'bio', 'profile_picture']
+
+    def validate_email(self, value):
+        if CustomUser.objects.filter(email=value).exists():
+            raise serializers.ValidationError("Email is already in use.")
+        return value
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+            bio=validated_data.get('bio', ''),
+        )
+        if 'profile_picture' in validated_data:
+            user.profile_picture = validated_data['profile_picture']
+            user.save()
+        Token.objects.create(user=user)
+        return user
