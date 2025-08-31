089976f5296dc9c5bbebb0f9e4adcfc4926b7066
Mohammed Naji Abdullah <abdullahmohammednaji@gmail.com>
2025-08-31 10:14:45 +0000

    a

---
+++ social_media_api/accounts/serializers.py
@@ -0,0 +1,38 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
+
+User = get_user_model()
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email',
+                  'bio', 'profile_picture', 'followers']
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'bio', 'profile_picture']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+            bio=validated_data.get('bio', ''),
+        )
+        if 'profile_picture' in validated_data:
+            user.profile_picture = validated_data['profile_picture']
+            user.save()
+        Token.objects.create(user=user)
+        return user
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
